2f059dc3a797a264ccccdda848d705b73f2abfa1
bernalportuguezjonathan-sft <bernalportuguezjonathan@email.com>
2026-03-16 21:15:13 -0500

    Agregando el dashboard del profesor y configurando plantillas

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -6,7 +6,8 @@ urlpatterns = [
     path('', views.iniciar_sesion, name='login_index'),
     path('registro/', views.registro_usuario, name='registro'),
     path('dashboard/', views.dashboard, name='dashboard'),
-    path('logout/', views.cerrar_sesion, name='logout'),
+    path('logout/', views.cerrar_sesion, name='cerrar_sesion'),
     path('editar/<str:leccion_id>/', views.editar_leccion, name='editar_leccion'),
     path('eliminar/<str:leccion_id>/', views.eliminar_leccion, name='eliminar_leccion'),
+    path('dashboard-profesor/', views.dashboard_profesor, name='dashboard_profesor'),
 ]
