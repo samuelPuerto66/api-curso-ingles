9a2ab753becc7ed20a1d92d43339beff2915c1f2
samuelPuerto66 <samuelpuerto066@gmail.com>
2026-03-18 10:23:38 -0500

    docs: mejorar documentación de configuración de usuarios

--- usuarios/apps.py
+++ usuarios/apps.py
@@ -2,4 +2,11 @@ from django.apps import AppConfig
 
 
 class UsuariosConfig(AppConfig):
+    """
+    Configuración de la aplicación de usuarios.
+    
+    Esta clase define la configuración principal para la app 'usuarios'
+    que maneja la autenticación, registro y gestión de usuarios.
+    """
     name = 'usuarios'
+    verbose_name = 'Gestión de Usuarios'
