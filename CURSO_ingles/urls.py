6c994cee462d1c9695bc2099d3a1d8a33b20eda7
bernalportuguezjonathan-sft <bernalportuguezjonathan@email.com>
2026-03-19 11:22:31 -0500

    Fix Firebase initialization and clean up settings/templates

--- CURSO_ingles/urls.py
+++ CURSO_ingles/urls.py
@@ -14,10 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django import views
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('usuarios.urls')),  
+    path('', include('usuarios.urls')),
 ]
